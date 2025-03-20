f81c6daec80fd42985bfd94307992e49cf967e22
Berina Majdancic <berina.majdancic.2019@gmail.com>
2025-03-20 16:33:38 +0100

    Add Damage Handling And Change Map Layout

--- Source/Scope/BaseCharacter.cpp
+++ Source/Scope/BaseCharacter.cpp
@@ -1,8 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BaseCharacter.h"
+#include "Engine/DamageEvents.h"
 #include "Kismet\GameplayStatics.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
 // Sets default values
 ABaseCharacter::ABaseCharacter()
 {
@@ -29,12 +31,17 @@ void ABaseCharacter::Shoot()
     GetWorld()->LineTraceSingleByChannel(HitResult, StartLocation, EndLocation, ECC_GameTraceChannel1);
     if (HitResult.bBlockingHit) {
         UE_LOG(LogTemp, Display, TEXT("Hit"));
-        if (MuzzleFlash)
-            UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), MuzzleFlash, HitResult.ImpactPoint, Rotation);
+        if (HitResult.GetActor() != this) {
+            if (ABaseCharacter* Actor = Cast<ABaseCharacter>(HitResult.GetActor())) {
+                if (MuzzleFlash)
+                    UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), MuzzleFlash, HitResult.ImpactPoint, Rotation);
+                FPointDamageEvent DamageEvent(BaseDamage, HitResult, -Rotation.Vector(), nullptr);
+                Actor->TakeDamage(BaseDamage, DamageEvent, GetController(), this);
+            }
+        }
     }
 }
-
-FRotator ABaseCharacter::GetAimRotation()
+FRotator ABaseCharacter::GetAimRotation() const
 {
     FVector StartLocation;
     FRotator Rotation;
@@ -47,6 +54,24 @@ FRotator ABaseCharacter::GetAimRotation()
         return (AimLocation.Rotation() - GetActorRotation());
     }
     return FRotator();
+}
+
+bool ABaseCharacter::GetIsDead() const
+{
+    return bIsDead;
+}
+
+float ABaseCharacter::TakeDamage(float Damage, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+    Super::TakeDamage(Damage, DamageEvent, EventInstigator, DamageCauser);
+    float DamageTaken = FMath::Min(Health, Damage);
+    UE_LOG(LogTemp, Display, TEXT("Damage Taken! %f"), DamageTaken);
+    Health -= DamageTaken;
+    if (Health == 0) {
+        Die();
+        UE_LOG(LogTemp, Display, TEXT("Dead!"));
+    }
+    return DamageTaken;
 }
 
 // Called when the game starts or when spawned
@@ -65,4 +90,17 @@ void ABaseCharacter::Tick(float DeltaTime)
 void ABaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
     Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void ABaseCharacter::Die()
+{
+    if (bIsDead)
+        return;
+    bIsDead = true;
+    if (APlayerController* PC = Cast<APlayerController>(GetController()))
+        DisableInput(PC);
+    GetCharacterMovement()->DisableMovement();
+    GetController()->UnPossess();
+    SetActorEnableCollision(false);
+    Destroy();
 }
